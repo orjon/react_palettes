3c8c56ebf6a318464d1c2eaedc2a445f1c177e50
orjon <github@orjon.com>
2020-04-07 10:37:49 +0100

    Color box styling

--- src/Palette.js
+++ src/Palette.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component {
   render(){
+    const colorBoxes = this.props.colors.map( color => 
+      <ColorBox background={color.color} name={color.name}/>
+    )
     return(
-      <div className='Palette'>Palette</div>
+      <div className='Palette'>
+        {/* nav */}
+        <div className='Palette-colors'>
+          {colorBoxes}
+        </div>
+        {/* footer */}
+      </div>
     )
   }
     
