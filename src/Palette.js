9ec78336f38f7708796797241299a9b378ab94db
orjon <github@orjon.com>
2020-04-07 09:54:22 +0100

    Created palette component

---
+++ src/Palette.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  render(){
+    return(
+      <div className='Palette'>Palette</div>
+    )
+  }
+    
+}
+
+export default Palette;
