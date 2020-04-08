fbeb14886ec82cd1cbf6b0df31596c4009210fb3
orjon <github@orjon.com>
2020-04-08 15:01:23 +0100

    Basic routes working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from './Palette';
+import { Route, Switch} from 'react-router-dom';
 import seedPalettes from './seedPalettes';
 import {generatePalette} from './colorHelpers';
 
-function App() {
-  console.log(generatePalette(seedPalettes[4]))
-  return (
-    <div className="App">
-      <Palette palette={generatePalette(seedPalettes[4])}/>
-    </div>
-  );
+class App extends Component {
+  findPalette(idToFind){
+    return seedPalettes.find(function(palette){
+      return palette.id === idToFind;
+    })
+  }
+  render(){
+    return (
+      <Switch>
+        <Route exact path='/' render={() => <h1>Palette list here</h1>} />
+        <Route
+          exact
+          path='/palette/:id'
+          render={(routeProps) => (
+            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>
+          )}
+        />
+      </Switch>
+  
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedPalettes[4])}/>
+      // </div>
+    )
+  }
 }
 
 export default App;
