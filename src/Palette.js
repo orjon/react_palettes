93150b540201b9e835423494765f07e4be8d15ff
orjon <github@orjon.com>
2020-04-08 08:57:55 +0100

    Slider styled

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css'; 
+// Place this before own styles!!
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+
 
 class Palette extends Component {
   constructor(props){
@@ -18,12 +20,16 @@ class Palette extends Component {
   }
 
   render(){
-    const colorBoxes = this.props.palette.colors[this.state.level].map( color => 
+    const {colors} = this.props.palette
+    const {level} = this.state
+    const colorBoxes = colors[level].map( color => 
       <ColorBox background={color.hex} name={color.name}/>
     )
     return(
       <div className='Palette'>
-        <Slider defaultValue={this.state.level} step={100} min={100} max={900} onAfterChange={this.changeLevel}/>
+        <div className='slider'>
+          <Slider defaultValue={level} step={100} min={100} max={900} onAfterChange={this.changeLevel}/>
+        </div>
         <div className='Palette-colors'>
           {colorBoxes}
         </div>
