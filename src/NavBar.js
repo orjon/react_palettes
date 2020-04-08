5d9550cc187c52139d668cb7b46441dd414fe826
orjon <github@orjon.com>
2020-04-08 12:13:28 +0100

    Snackbar added. Color format selector styled.

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
 import 'rc-slider/assets/index.css'; 
 // Place this before own styles!!
 import './NavBar.css';
@@ -10,44 +11,62 @@ class NavBar extends Component {
   constructor(props){
     super(props);
     this.state= {
-      format: 'hex'
+      format: 'hex',
+      snackbarOpen: false
     }
-    this.handleChange = this.handleChange.bind(this);
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
 
-  handleChange(e){
-    this.setState({format: e.target.value});
+  handleFormatChange(e){
+    this.setState({format: e.target.value, snackbarOpen: true});
     this.props.handleChange(e.target.value);
   }
+
+  closeSnackbar(){
+    this.setState({snackbarOpen: false})
+  }
+
   render(){
     const {level, changeLevel} = this.props
     return(
-      <header className='NavBar'>
-        <div className='logo'>
-          <a href='http://www.orjon.com/'>Palettes</a>
-        </div>
-        <div className='slider-level'>
-            LEVEL: {level}
-        </div>
-        <div className='slider'>
-          <Slider
-            defaultValue={level}
-            step={100}
-            min={100}
-            max={900}
-            onAfterChange={changeLevel}/>
-        </div>
-        <div className='select-container'>
-        <Select 
-          value={this.state.format}
-          onChange={this.handleChange}>
-          <MenuItem value='hex'>HEX - #ffffff</MenuItem>
-          <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
-          <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0)</MenuItem>
-        </Select>
-        </div>
-        
-      </header>
+      <div>
+        <header className='NavBar'>
+          <div className='logo'>
+            <a href='http://www.orjon.com/'>Palettes</a>
+          </div>
+          <div className='slider-level'>
+              LEVEL: {level}
+          </div>
+          <div className='slider'>
+            <Slider
+              defaultValue={level}
+              step={100}
+              min={100}
+              max={900}
+              onAfterChange={changeLevel}/>
+          </div>
+          <div className='select-container'>
+          <Select 
+            value={this.state.format}
+            onChange={this.handleFormatChange}>
+            <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+            <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
+          </div>
+        </header>
+        <footer>
+          <Snackbar
+            autoHideDuration={2500}
+            onClick={this.closeSnackbar}
+            onClose={this.closeSnackbar}
+            anchorOrigin={{vertical:'bottom', horizontal:'left'}}
+            open={this.state.snackbarOpen}
+            message={<span>Format changed to {this.state.format.toUpperCase()}</span>}/>
+        </footer>
+      </div>
+
     )
   }
   
