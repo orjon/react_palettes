a35accf797b4f9d36cdc470c0be8a5c90e241285
orjon <github@orjon.com>
2020-04-15 12:17:07 +0100

    Added color picker and buttons

--- src/PaletteNew.js
+++ src/PaletteNew.js
@@ -11,9 +11,10 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { ChromePicker } from 'react-color';
+import Button from '@material-ui/core/Button';
 
-
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = theme => ({
   root: {
@@ -130,7 +131,17 @@ class PaletteForm extends Component {
             </IconButton>
           </div>
           <Divider />
+          <Typography variant='h4'>New Palette</Typography>
+          <div>
+            <Button variant='contained' color='secondary'>Clear Palette</Button>
+            <Button variant='contained' color='primary'>Random Color</Button>
+          </div>
 
+          <ChromePicker
+            color='white'
+            onChangeComplete={(newColor) => console.log(newColor)}
+          />
+          <Button variant='contained' color='primary'>Add Color</Button>
         </Drawer>
         <main
           className={classNames(classes.content, {
